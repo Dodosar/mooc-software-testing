0ce7a18463d63c0f08be0ecb1f5da0ece1f638a2
Dodosar <Lbvf650065>
2019-02-04 10:50:38 -0500

    MOCK test

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -17,4 +17,36 @@ public class CountLettersTest {
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void multipleMatchingWords1() {
+        int words = new CountLetters().count("cats|dogs|pets");
+        Assertions.assertEquals(3, words);
+    }
+
+    @Test
+    public void multipleMatchingWords2() {
+        int words = new CountLetters().count("cats|dogss");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void multipleMatchingWords3() {
+        int words = new CountLetters().count("cats|dogr|testx");
+        Assertions.assertEquals(3, words);
+    }
+
+    @Test
+    public void multipleMatchingWords4() {
+        int words = new CountLetters().count("cats|dogr|");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void multipleMatchingWords5() {
+        int words = new CountLetters().count("cats|dog|");
+        Assertions.assertEquals(1, words);
+    }
+
+
+
 }
